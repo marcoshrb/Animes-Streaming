8b28acdc39cbc2d80007a12d7ec1ff88569b21dd
marcos <marcoshr_baptista@hotmail.com>
2024-05-23 08:56:09 -0300

    conflitos

--- Front/src/components/Navbar2/index.tsx
+++ Front/src/components/Navbar2/index.tsx
@@ -8,7 +8,7 @@ import { useNavigate } from 'react-router-dom';
 function NavBarComponent() {
 
   const navigate = useNavigate();
-  
+
   function Home() {
     navigate('../home')
   }
@@ -19,24 +19,20 @@ function NavBarComponent() {
   return (
     <Navbar className={style["navbar"]} bg="dark" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand href="#home"><img src='../../../src/assets/img/LogoT.png' width={"150px"}/></Navbar.Brand>
+        <Navbar.Brand href="#home"><img src='../../../src/assets/img/LogoT.png' width={"150px"} /></Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-          <Button variant="dark" onClick={Home}>Home</Button>
-          <Button variant="dark" onClick={AddMovie}>Adicionar Filme</Button>
-        <Navbar.Brand className={style.drop}>
-        <NavDropdown title="Configurações" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
+
+          <Navbar.Brand className={style.drop}>
+            <NavDropdown title="Configurações" id="basic-nav-dropdown">
+              <NavDropdown.Item >
+                <Button variant="dark" onClick={Home}>Home</Button>
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
+              <NavDropdown.Item >
+                <Button variant="dark" onClick={AddMovie}>Adicionar Filme</Button>
               </NavDropdown.Item>
             </NavDropdown>
-        </Navbar.Brand>
+          </Navbar.Brand>
           <Navbar.Text>
             Signed in as: <a href="#login">Mark Otto</a>
           </Navbar.Text>
